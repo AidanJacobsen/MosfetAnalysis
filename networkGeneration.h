21aa381bb70462dacc0a44d64674c4700b81e39d
AidanJacobsen <aidanmjacobsen@gmail.com>
2023-10-08 20:13:58 -0400

    add network recursive generation

--- networkGeneration.h
+++ networkGeneration.h
@@ -10,6 +10,9 @@
 Node* initializeNode();
 void initializeNetwork();
 Mosfet* generateMosfet(MosfetList* mList, Node* inputNode, Node* gateNode, Node* outputNode, bool type);
-
+void removeMosfet(MosfetList* mosfetList, Mosfet* targetMosfet);
+void generateAllCombinations(NodeList* nodeList, MosfetList* mosfetList, int * combinationCounter, int mosfetsLeft);
+bool addNode(NodeList* nodeList);
+removeNode(NodeList* nodeList, Node* targetNode);
 
 #endif  // NETWORK_GENERATION_H
